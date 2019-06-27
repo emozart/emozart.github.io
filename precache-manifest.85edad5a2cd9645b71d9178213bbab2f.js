5427b414a9497f7fe3bec25bfba8bf62c4c0596b
Elton Mozart <elton.mozart@gmail.com>
2019-06-26 22:47:36 -0300

    Publicação inicial
    Publicação inicial do site do projeto PI Univesp

---
+++ precache-manifest.85edad5a2cd9645b71d9178213bbab2f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ea51f22f5b1acc46fe9",
+    "url": "/static/css/main.c48f93b0.chunk.css"
+  },
+  {
+    "revision": "5ea51f22f5b1acc46fe9",
+    "url": "/static/js/main.77d8a45d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2850c1dee8202808f6e",
+    "url": "/static/js/2.36b3dfba.chunk.js"
+  },
+  {
+    "revision": "c281f024381858e041c371ad10477834",
+    "url": "/static/media/Card_01.c281f024.jpg"
+  },
+  {
+    "revision": "bb49e40a220c5ff0e00bd2134d00adb8",
+    "url": "/static/media/Card_02.bb49e40a.jpg"
+  },
+  {
+    "revision": "5930c2570b230021ed5b1a36b837854d",
+    "url": "/static/media/Card_03.5930c257.jpg"
+  },
+  {
+    "revision": "ef51ba9e76f689057ac4f0c9d6870e1e",
+    "url": "/static/media/Carousel03.ef51ba9e.png"
+  },
+  {
+    "revision": "68f8369c181f4085336a5f14cd988342",
+    "url": "/static/media/Carousel02.68f8369c.jpg"
+  },
+  {
+    "revision": "11f6f8072df5f9a537e9eb58a7de5b51",
+    "url": "/index.html"
+  }
+];
